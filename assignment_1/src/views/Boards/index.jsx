70bc6fbae767e7381b170d260f0913a9c1c3a294
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-26 17:28:07 +0000

    Board list setup

--- assignment_1/src/views/Boards/index.jsx
+++ assignment_1/src/views/Boards/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { ScrollView, View, Text, Image } from 'react-native';
 // import Toolbar from '../../components/Toolbar';
 // import GalleryList from '../../components/GalleryList';
 import ImageThumbnail from '../../components/ImageThumbnail';
@@ -12,15 +12,18 @@ const boardsList = data.boards;
 class Boards extends React.Component {
   render() {
     return (
-      <View style={styles.board}>
+      <ScrollView style={styles.container}>
+        <Text style={styles.type}>Board List</Text>
         { boardsList.map(function(item,index) {
           return(
-            <View><Text>{item.name}</Text>
-            <ImageThumbnail file={item.thumbnailPhoto}/>
+            <View style={styles.board}>
+              <ImageThumbnail file={item.thumbnailPhoto}/>
+              <Text style={styles.title}>{item.name}</Text>
+              <Text style={styles.description}>{item.description}</Text>
             </View>
           )}
         )}
-      </View>
+      </ScrollView>
     );
   }
 };
--- assignment_1/src/views/Boards/styles.js
+++ assignment_1/src/views/Boards/styles.js
@@ -1,10 +1,32 @@
 import {StyleSheet} from 'react-native';
 import { blue, lightyellow, babyblue,lightestblue } from '../../styles/colors';
 export default StyleSheet.create({
-
-  board:{
-    borderWidth: 2,
+  container: {
+    flex:1,
+    padding:30,
+    backgroundColor:lightestblue,
   },
+  type:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign:'center',
 
+  },
+  board:{
+    flex: 4,
+    width:300,
+    marginTop:15,
+    padding:20,
+    borderRadius:12,
+    backgroundColor:'white',
+  },
+  title:{
+    paddingTop:10,
+    fontWeight: 'bold',
+  },
+  description:{
+    paddingTop:10,
+
+  }
 
 });
