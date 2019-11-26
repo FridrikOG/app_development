5aa4b0d60aa120198d829ccc726b7280952656d7
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-26 11:58:47 +0000

    added colors

--- assignment_1/src/styles/colors.js
+++ assignment_1/src/styles/colors.js
@@ -1,2 +1,4 @@
-export const lightblue = '#13CDC1';
+export const blue = '#13CDC1';
 export const lightyellow = '#F6F096';
+export const babyblue = '#96F6F6';
+export const lightestblue = '#BBFAFA';
--- assignment_1/src/views/Main/styles.js
+++ assignment_1/src/views/Main/styles.js
@@ -1,12 +1,12 @@
 import {StyleSheet} from 'react-native';
-import { lightblue, lightyellow } from '../../styles/colors';
+import { blue, lightyellow, babyblue,lightestblue } from '../../styles/colors';
 export default StyleSheet.create({
   container: {
     flex:1,
     padding:30,
     alignItems: 'center',
     justifyContent: 'space-around',
-    backgroundColor: lightyellow,
+    backgroundColor: lightestblue,
   },
   title: {
     fontSize: 40,
@@ -23,7 +23,7 @@ export default StyleSheet.create({
     paddingRight: 20,
     //width:120,
     height:65,
-    backgroundColor: lightblue,
+    backgroundColor: blue,
     borderRadius: 12,
     //borderColor: 'white',
     //borderWidth: 2,
