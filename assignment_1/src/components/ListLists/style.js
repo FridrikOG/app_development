1650f81f1ace75c6f9233a0ec4865a017f2ee274
Friðrik Gunnarsson <fridrik@gmx.com>
2019-11-26 16:56:20 +0000

    soemthing works
    lol

--- assignment_1/src/components/ListLists/style.js
+++ assignment_1/src/components/ListLists/style.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  listContainer: {
-    flex: 1,
+  lists: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    padding: 30,
   },
 });
--- assignment_1/src/views/Lists/index.jsx
+++ assignment_1/src/views/Lists/index.jsx
@@ -3,15 +3,24 @@ import { View, Text } from 'react-native';
 import Toolbar from '../../components/Toolbar';
 import data from '../../resources/data.json';
 import ListLists from '../../components/ListLists';
+import styles from './styles';
+const boardsList = data.lists;
 
 const Lists = () => (
   <View>
     <Toolbar />
     <Text style={{ padding: 100, textAlign: 'center' }}>
-        You're in our lists
+        You're in our list
+
+      { boardsList.map(function(item,index) {
+        
+        return ( <Text style={styles.lists}> Item ID: {item.id} Name: {item.name} 
+        color code: {item.color} boardId: {item.boardid}</Text>
+        
+        )
+      }
+      )}
     </Text>
-    <ListLists images={ data.board}/>
-    <Text>  above should be datafsdafdsf</Text>
   </View>
 );
 
