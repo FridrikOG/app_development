c81daa8d262780679185d4f23157fd50a0d23b9d
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-26 14:48:04 +0000

    board changes
    npm install react-navigation react-navigation-stack react-native-gesture-handler

--- assignment_1/src/views/Boards/index.jsx
+++ assignment_1/src/views/Boards/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 // import Toolbar from '../../components/Toolbar';
 // import GalleryList from '../../components/GalleryList';
 import data from '../../resources/data.json';
 
-const Boards = () => (
+class Boards extends React.Component {
+  render() {
+    return (
       <View>
-        <Text style={{ padding: 100, textAlign: 'center' }}>
-        This is our board list
-        </Text>
+        <Text style={{ padding: 100, textAlign: 'center' }}>Nothin to see here</Text>
+        <FlatList
+            numColumns={ 3 }
+            data={ data.boards }
+        />
+
       </View>
-);
+    )
+  }
+};
+
 
 export default Boards;
