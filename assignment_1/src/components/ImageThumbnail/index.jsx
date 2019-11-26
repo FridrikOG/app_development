326e0a000a2e95d6b29bb9ac499f739e7837d5fd
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-26 16:46:40 +0000

    Thumbnails working

--- assignment_1/src/components/ImageThumbnail/index.jsx
+++ assignment_1/src/components/ImageThumbnail/index.jsx
@@ -1,1 +1,11 @@
-dsad
+import React from 'react';
+import { Image } from 'react-native';
+
+const ImageThumbnail = ({ file }) => (
+  <Image
+    style={{width:50,height:50}}
+    resizeMode="cover"
+    source={{uri:file}} />
+);
+
+export default ImageThumbnail;
---
+++ assignment_1/src/views/Boards/styles.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import { blue, lightyellow, babyblue,lightestblue } from '../../styles/colors';
+export default StyleSheet.create({
+
+  board:{
+    borderWidth: 2,
+  },
+
+
+});
