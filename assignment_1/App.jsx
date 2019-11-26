8d298a60ce2febbe7ebcd3c21119065b921a10e3
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-26 12:19:30 +0000

    Boards added

--- assignment_1/App.jsx
+++ assignment_1/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Main from './src/views/Main';
+import Boards from './src/views/Boards';
 
 export default function App() {
   return (
-    <Main />
+    <Boards />
   );
 }
---
+++ assignment_1/src/views/Boards/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import  {View, Text} from 'react-native';
+//import Toolbar from '../../components/Toolbar';
+//import GalleryList from '../../components/GalleryList';
+import data from '../../resources/data.json';
+
+class Boards extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text style={{padding:100,textAlign:'center'}}>This is our board list</Text>
+      </View>
+    )
+  }
+};
+
+export default Boards;
