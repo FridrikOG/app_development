8e28549cca1b8106f396a3a006873fd3f337387a
Friðrik Gunnarsson <fridrik@gmx.com>
2019-11-27 19:43:53 +0000

    Keeping track of id's

--- assignment_1/src/views/Boards/index.jsx
+++ assignment_1/src/views/Boards/index.jsx
@@ -6,16 +6,34 @@ import styles from './styles';
 import Toolbar from '../../components/Toolbar';
 
 class Boards extends React.Component {
-  fffuck(){
-    console.log("well fuck");
+  state = {
+    // All images within the application directory
+    ids: [],
+    // All selected images
+    selectedIds: [],
+}
 
-  }
+  onBoardPress(id){
+    const {selectedIds} = this.state;
+    
+    if (selectedIds.includes(id) == false){
+      console.log("Not there")
+      selectedIds.push(id)
+    } else{
+      console.log('is there')
+      let indexId = selectedIds.indexOf(id)
+      selectedIds[indexId] = null 
+      //this.setState({selectedIds: selectedIds.filter(x => x !== id)})
+      }
+      console.log("LOGGING ARRAY: ",selectedIds)
+      }
+
 
   render() {
     const props = this.props;
-    handler = () =>{
+    handler = (id) =>{
       console.log("YOU MADE IT");
-      this.fffuck();
+      this.onBoardPress(id);
 
     }
     return (
@@ -25,15 +43,15 @@ class Boards extends React.Component {
         <Text style={styles.type}>Board List</Text>
 
         {data.boards.map(function(item,index){
-          onBoardLongPress = () => {
+          onBoardLongPress = (id) => {
             console.log("THIS IS A LONG ONE");
-            this.handler();
+            this.handler(id);
           };
           console.log("ID in BOARDS: ",item.id);
           return(
             <TouchableOpacity
-            onLongPress={this.onBoardLongPress}
-            onPress={() => {props.navigation.navigate('Lists',{boardId: props.boardId, name: props.name})}}
+            onPress={() => {this.onBoardLongPress(item.id)}}
+            onLongPress={() => {props.navigation.navigate('Lists',{boardId: props.boardId, name: props.name})}}
             style={styles.button}>
               <BoardList boardId={item.id} images={item.thumbnailPhoto} name={item.name} description={item.description} navigate={props.navigation.navigate}/>
             </TouchableOpacity>
