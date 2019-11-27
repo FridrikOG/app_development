64186137041535e26635e4c375a33e8f478a4889
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-27 21:37:45 +0000

    File services and lists with flatlist

---
+++ assignment_1/src/components/ListLists/styles.js
@@ -0,0 +1,42 @@
+import { StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import { blue, lightyellow, babyblue,lightestblue } from '../../styles/colors';
+
+export default StyleSheet.create({
+  container: {
+
+  },
+  title: {
+    backgroundColor: lightyellow,
+    padding:20,
+    fontSize:20,
+  },
+  text: {
+    borderColor:'black',
+    borderWidth:1,
+    borderRadius:12,
+    fontSize:15,
+    margin: 15,
+    padding:20,
+  },
+  finished: {
+    borderColor:'green',
+    borderWidth:3,
+    borderRadius:12,
+    fontSize:15,
+    margin: 15,
+    padding:20,
+  },
+
+  lists: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    padding: 10,
+    borderRadius: 4,
+    borderWidth: 5,
+    borderColor: 'red',
+  },
+  listsContainer: {
+    borderColor: 'red'
+  }
+});
--- assignment_1/src/views/Lists/index.jsx
+++ assignment_1/src/views/Lists/index.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
 import { ScrollView, View, Text } from 'react-native';
 import Toolbar from '../../components/Toolbar';
+import ListLists from '../../components/ListLists';
 import data from '../../resources/data.json';
 import styles from './styles';
 import getParameters from '../../components/Parameters/getParameters';
 
 class Lists extends React.Component{
+  state = {
+    lists: data.lists,
+    selectedLists: [],
+  }
+
   render(){
     const props = this.props.navigation.state.params;
+    const { lists, selectedLists } = this.state;
+    const toShow = []
     console.log(props);
     console.log('ID in LISTS: ',props.boardId);
-
+    console.log("TO SHOW: ", toShow)
     return(
       <ScrollView>
         <Toolbar />
         <Text style={styles.title}>Currently in Board: {props.boardId}</Text>
-          { data.lists.map(function(item,index) {
-            if (item.boardId == props.boardId){
-              return (
-                <View style={styles.container}>
-                  <Text style={[styles.text, {backgroundColor:item.color}]}>
-                    List: {item.id} {"\n"}
-                    Name: {item.name} {"\n"}
-                    Color code: {item.color} {"\n"}
-                  </Text>
-                </View>
-              )
-            }
-          }
-          )
-        }
+        {lists.map(function(item,index){
+          if (item.boardId == props.boardId){
+            console.log("ITEM ID: ", item.boardId, props.boardId, item);
+            toShow.push(item);
 
+          }
+        })}
+        <ListLists lists={toShow}/>
       </ScrollView>
 
     )
