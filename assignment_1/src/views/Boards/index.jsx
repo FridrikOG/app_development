2e7983d8d72b846f97c72f73d444c2738d29f208
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-11-27 17:04:50 +0000

    Different scopes functions

--- assignment_1/src/views/Boards/index.jsx
+++ assignment_1/src/views/Boards/index.jsx
@@ -1,27 +1,45 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text, Image, TouchableHighlight } from 'react-native';
+import { ScrollView, View, Text, Image, TouchableOpacity } from 'react-native';
 import data from '../../resources/data.json';
 import BoardList from '../../components/BoardList';
 import styles from './styles';
 import Toolbar from '../../components/Toolbar';
 
 class Boards extends React.Component {
-  onBoardLongPress(name) {
+  fffuck(){
+    console.log("well fuck");
+
   }
 
   render() {
     const props = this.props;
+    handler = () =>{
+      console.log("YOU MADE IT");
+      this.fffuck();
+
+    }
     return (
 
+
       <ScrollView style={styles.container}>
-        <Text></Text>
         <Text style={styles.type}>Board List</Text>
+
         {data.boards.map(function(item,index){
+          onBoardLongPress = () => {
+            console.log("THIS IS A LONG ONE");
+            this.handler();
+          };
           console.log("ID in BOARDS: ",item.id);
           return(
-            <BoardList boardId={item.id} images={item.thumbnailPhoto} name={item.name} description={item.description} navigate={props.navigation.navigate}/>
+            <TouchableOpacity
+            onLongPress={this.onBoardLongPress}
+            onPress={() => {props.navigation.navigate('Lists',{boardId: props.boardId, name: props.name})}}
+            style={styles.button}>
+              <BoardList boardId={item.id} images={item.thumbnailPhoto} name={item.name} description={item.description} navigate={props.navigation.navigate}/>
+            </TouchableOpacity>
           )}
         )}
+
       </ScrollView>
 
     )
