89a761dbd9205e42db9eddffeb9cd12afe00d47c
Friðrik Gunnarsson <fridrik@gmx.com>
2019-11-28 11:30:29 +0000

    Opacity and checkmark for selected

--- assignment_1/src/components/BoardList/index.jsx
+++ assignment_1/src/components/BoardList/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { View,Text, FlatList, TouchableOpacity } from 'react-native';
 import ImageThumbnail from '../ImageThumbnail';
 import styles from './styles';
+import {AntDesign} from '@expo/vector-icons'
 
 const BoardList = ({ boards, props, onLongPress, selectedIds }) => {
     console.log("Logging in props: ", props)
     console.log("LOGGING ID's in board " , selectedIds)
-    
     
     return (
         <View style={styles.container}>
@@ -17,15 +17,19 @@ const BoardList = ({ boards, props, onLongPress, selectedIds }) => {
             extraData={selectedIds}
             renderItem={( {item: {name, description, thumbnailPhoto, id }}) => {
                 console.log(selectedIds.indexOf(id) !== -1)
+                const isSelected = selectedIds.indexOf(id) !== -1
                 //isSelected = false 
                 return (
                     <TouchableOpacity onLongPress={() => onLongPress(id)} onPress={() => props.navigation.navigate('Lists', {boardId:boards.id, name:boards.name})}>
+                    {isSelected ? <AntDesign name = "checkcircleo"/> : <></>}
                     <View style={styles.boards} >
+                        <View style={{opacity: isSelected ? 0.5 : 1}} >
                         <ImageThumbnail file={thumbnailPhoto} />
                         <Text style={styles.title}> {name} </Text>
                         <Text style={styles.description}>{description} </Text>
                         <Text style={styles.description}>{id} </Text>
-                        <Text> {selectedIds.indexOf(id) !== -1 ? 'Selected' : 'Not selected'}</Text>
+                        <Text> {isSelected ? 'Selected' : 'Not selected'}</Text>
+                        </View>
                     </View>
                     </TouchableOpacity>
                 )
