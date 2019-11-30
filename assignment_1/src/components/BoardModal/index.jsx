6549476c8122e5cdb2d3cef2d15f1e1385c3aa3a
Friðrik Gunnarsson <fridrik@gmx.com>
2019-11-30 14:37:36 +0000

    BoardModal index.jsx linted

--- assignment_1/src/components/BoardModal/index.jsx
+++ assignment_1/src/components/BoardModal/index.jsx
@@ -1,110 +1,122 @@
 import React from 'react';
 import NativeModal from 'react-native-modal';
-import { View, Text, StyleSheet, TextInput, Picker, TouchableOpacity, ScrollView } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, ScrollView,
+} from 'react-native';
+import PropTypes from 'prop-types';
 import styles from './styles';
 
 class BoardModal extends React.Component {
+  // eslint-disable-next-line react/state-in-constructor
   state = {
-  name: '',
-  description: '',
-  thumbnailPhoto:'',
-  isValid: false,
-  nameRequired: ''
-}
-  handleSubmit = () => {
-    const value = this._form.getValue(); // use that ref to get the form value
-  }
-  updateName(name){
-    // Name of board has to be at least 3 characters
-    if (name.length > 2){
-      this.setState({isValid: true})
-    // If name of board becomes less than 3 characters we make the form invalid for submission
-    }
-    else {
-      this.setState({isValid: false})
-    }
-    // Actually updating the name to the state
-    this.setState({name});
+    name: '',
+    description: '',
+    thumbnailPhoto: '',
+    isValid: false,
+    nameRequired: '',
   }
 
-  updateDescription(description){
-    this.setState({description});
+  updateName(name) {
+    // Name of board has to be at least 3 characters
+    if (name.length > 2) {
+      this.setState({ isValid: true });
+    // If name of board becomes less than 3 characters we make the form invalid for submission
+    } else {
+      this.setState({ isValid: false });
+    }
+    // Actually updating the name to the state
+    this.setState({ name });
   }
-  determineErrorMsg(){
-    const {isValid} = this.state;
-    if(isValid == false){
-      this.setState({nameRequired: 'Name of the board must be more than two characters.'});
-      }
-    else{
-      this.setState({nameRequired: ''});
+
+  updateDescription(description) {
+    this.setState({ description });
+  }
+
+  determineErrorMsg() {
+    const { isValid } = this.state;
+    if (isValid === false) {
+      this.setState({ nameRequired: 'Name of the board must be more than two characters.' });
+    } else {
+      this.setState({ nameRequired: '' });
     }
   }
-  cleanUp(Submit){
-    const {closeModal,addBoard} = this.props;
+
+  cleanUp(Submit) {
+    const { closeModal, addBoard } = this.props;
     // Clearing the error messages
     this.setState({
       name: '',
       description: '',
-      thumbnailPhoto:'',
+      thumbnailPhoto: '',
       isValid: false,
-      nameRequired: ''
-    })
+      nameRequired: '',
+    });
     // If Submit was pressed we add the board to our data
-    if(Submit){
-      addBoard(this.state)
-    }
-    else{
+    if (Submit) {
+      addBoard(this.state);
+    } else {
     // GoBack was pressed - Closing the model after clearing the error message
-    closeModal();
+      closeModal();
     }
-    }
+  }
 
   render() {
-    const { isOpen, closeModal} = this.props;
-    const {isValid,nameRequired} = this.state;
+    const { isOpen, closeModal } = this.props;
+    const {
+      description, name, isValid, nameRequired,
+    } = this.state;
+    const bool = true;
 
-    return(
-    <NativeModal
-      isVisible={isOpen}
-      hasBackdrop
-      onBackButtonPress={closeModal}
-      onSwipeComplete={closeModal}
-      SwipeDirection={['up','down']}
-      style={styles.modal}>
-      <ScrollView style={[styles.container]}>
-        <Text style={styles.title}>Creating a New Board</Text>
-        <Text style={{color:'red'}}>{nameRequired}</Text>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Name"
-          placeholderTextColor="black"
-          value={this.state.name}
-          onChangeText={ (text) => this.updateName(text)}/>
-        <TextInput
-          style={[styles.textInput, {height:200}]}
-          placeholder="Description (optional)"
-          placeholderTextColor="black"
-          editable={true}
-          multiline = {true}
-          value={this.state.description}
-          onChangeText={ text => this.updateDescription(text)}/>
+    return (
+      <NativeModal
+        isVisible={isOpen}
+        hasBackdrop
+        onBackButtonPress={closeModal}
+        onSwipeComplete={closeModal}
+        SwipeDirection={['up', 'down']}
+        style={styles.modal}
+      >
+        <ScrollView style={[styles.container]}>
+          <Text style={styles.title}>Creating a New Board</Text>
+          <Text style={{ color: 'red' }}>{nameRequired}</Text>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Name"
+            placeholderTextColor="black"
+            value={name}
+            onChangeText={(text) => this.updateName(text)}
+          />
+          <TextInput
+            style={[styles.textInput, { height: 200 }]}
+            placeholder="Description (optional)"
+            placeholderTextColor="black"
+            editable={bool}
+            multiline={bool}
+            value={description}
+            onChangeText={(text) => this.updateDescription(text)}
+          />
 
-        <View style={{flexDirection:'row'}}>
-          <TouchableOpacity
-          style={styles.button}
-          onPress={isValid ? () => this.cleanUp(true): () => this.determineErrorMsg()}>
-          <Text style={styles.btntxt}>Submit</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => this.cleanUp(false)}>
-            <Text style={styles.btntxt}>Go Back</Text>
-          </TouchableOpacity>
-
-          
-        </View>
-      </ScrollView>
-    </NativeModal>
+          <View style={{ flexDirection: 'row' }}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={isValid ? () => this.cleanUp(true) : () => this.determineErrorMsg()}
+            >
+              <Text style={styles.btntxt}>Submit</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={() => this.cleanUp(false)}>
+              <Text style={styles.btntxt}>Go Back</Text>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
+      </NativeModal>
     );
   }
 }
+
+BoardModal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  closeModal: PropTypes.func.isRequired,
+  addBoard: PropTypes.func.isRequired,
+};
 
 export default BoardModal;
