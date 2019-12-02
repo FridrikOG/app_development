da306597db28ca1ba69c0c8773b9172fe57d7d5e
Thorarinn S Gunnarsson <torarinn91@gmail.com>
2019-12-02 15:24:32 +0000

    Revert "I try, but fail :-1:"
    This reverts commit 22f58007afbd28004823d15bfd45920f85078655.

--- Assignment_2/src/views/Main/index.jsx
+++ Assignment_2/src/views/Main/index.jsx
@@ -11,7 +11,7 @@ class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ImageBackground style={styles.backgroundImage} source={{ uri: 'https://ph-files.imgix.net/651adc69-1ed9-47b3-8f6f-3a83ed0e2322?auto=format%2Ccompress%2Cenhance&dpr=2&crop=false&fit=max&w&h=500' }}>
+        <ImageBackground style= { styles.backgroundImage } source={{uri: 'https://ph-files.imgix.net/651adc69-1ed9-47b3-8f6f-3a83ed0e2322?auto=format%2Ccompress%2Cenhance&dpr=2&crop=false&fit=max&w&h=500'}}>
           <View style={styles.logoBorder}>
             <Image style={styles.logo} source={logo} />
           </View>
