79e706aa72b418da46b45b2afc40245264facc01
Friðrik Gunnarsson <fridrik@gmx.com>
2019-12-02 12:43:02 +0000

    Reads out data in contact list

--- Assignment_2/src/views/Contacts/index.jsx
+++ Assignment_2/src/views/Contacts/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/state-in-constructor */
 /* eslint-disable no-undef */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
@@ -6,7 +7,7 @@ import data from '../../resources/data.json';
 // import Toolbar from '../../components/Toolbar';
 // import data from '../../resources/data';
 // import AdministratorList from '../../components/AdministratorList';
-
+import ContactList from '../../components/ContactList/ContactList';
 
 class Contacts extends React.Component {
   // const props = this.props.navigation.state.params;
@@ -14,10 +15,14 @@ class Contacts extends React.Component {
     contacts: data.contacts,
   };
 
-
   render() {
+    const {
+      contacts,
+    } = this.state;
+    console.log(contacts);
     return (
       <View>
+        <ContactList boards={contacts} />
         <Text> le goooooo</Text>
       </View>
     );
