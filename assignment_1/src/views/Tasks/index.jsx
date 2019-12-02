e91fa6d8ecce6a1e43e6035702bdb7908b70aceb
Friðrik Gunnarsson <fridrik@gmx.com>
2019-12-02 10:16:31 +0000

    Assignment 2 added

--- assignment_1/src/views/Tasks/index.jsx
+++ assignment_1/src/views/Tasks/index.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable object-curly-spacing */
+/* eslint-disable react/sort-comp */
 import React from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import Toolbar from '../../components/Toolbar';
 import data from '../../resources/data';
 import TaskList from '../../components/TaskList';
@@ -8,36 +10,33 @@ import ModifyTask from '../../components/CreateTask/ModifyTaskModal';
 import styles from './styles';
 
 // Class takes care of the tasks
-class Tasks extends React.Component{
+class Tasks extends React.Component {
+  // eslint-disable-next-line react/state-in-constructor
   state = {
-    // Takes the tasks straight from the data since boards/lists aren't modifying them
     tasks: data.tasks,
     selectedIds: [],
     isOpenTaskModal: false,
     isModifyOpen: false,
-
   }
 
   // Called from the create task modal
   addTask = (info) => {
-    const { tasks, isOpenTaskModal } = this.state;
-    this.setState({ tasks: [ ...tasks, info ], isOpenTaskModal: false });
-
+    const { tasks } = this.state;
+    this.setState({ tasks: [...tasks, info], isOpenTaskModal: false });
   }
   // Uses our selection system
   onTaskLongPress(id){
-    const {selectedIds} = this.state;
-    if (selectedIds.indexOf(id) !== -1){
+    const {selectedIds } = this.state;
+    if (selectedIds.indexOf(id) !== -1) {
       // ID is already within the list
       this.setState({
-        selectedIds: selectedIds.filter(x => x !== id)
+        selectedIds: selectedIds.filter((x) => x !== id),
       });
-    } else{
+    } else {
       // IDs need to be added
       this.setState({
-        selectedIds: [...selectedIds, id]
-      })
-
+        selectedIds: [...selectedIds, id],
+      });
     }
   }
 
@@ -46,31 +45,31 @@ class Tasks extends React.Component{
     const {selectedIds,tasks} = this.state;
     this.setState({
       // Only retrieve images which were NOT part of the selected images list
-      tasks: tasks.filter(task => selectedIds.indexOf(task.id) == -1),
+      tasks: tasks.filter((task) => selectedIds.indexOf(task.id) == -1),
       selectedIds: [],
-    })}
+    }); }
 
   // This one should display a caption whenever someone selects a board
   // How this one also shows if it should be plural or not
   displayCaption() {
-    const { selectedIds } = this.state
-    if (selectedIds.length === 0) { return;}
+    const { selectedIds } = this.state;
+    if (selectedIds.length === 0) { return; }
 
     let itemCaption = 'tasks';
     if (selectedIds.length === 1) {
       itemCaption = 'task';
     }
-    return <Text>You have {selectedIds.length} selected {itemCaption} </Text>
+    return <Text>You have {selectedIds.length} selected {itemCaption} </Text>;
 }
   updateTask(info) {
     // const { tasks, isModifyOpen, lists } = this.props;
     const lists = this.props.navigation.state.params.lists;
-    const { tasks, isModifyOpen, selectedIds } = this.state;
+    const { tasks, selectedIds } = this.state;
 
     const oldTask = tasks.filter((x) => x.id === selectedIds[0]);
     // Making sure if the name submitted is the old one then we fix it
     if (info.name == '') {
-      info.name = oldTask[0].name
+      info.name = oldTask[0].name;
     }
     if (info.description == '') {
       info.description = oldTask[0].description;
@@ -92,13 +91,11 @@ class Tasks extends React.Component{
           'description': info.description,
           // eslint-disable-next-line quote-props
           'finished': info.finished,
-          'listId' : listId
+          'listId': listId,
         };
 
         const newTasks = tasks.filter((task) => selectedIds.indexOf(task.id) === -1);
         this.setState({ tasks: [...newTasks, newTask], isModifyOpen: false, selectedIds: [] });
-
-
       }
     //const newLists = lists.filter((list) => selectedIds.indexOf(list.id) === -1);
     //this.setState({ lists: [...newLists, newList], isModifyModalOpen: false, selectedIds: [] });
@@ -110,8 +107,10 @@ class Tasks extends React.Component{
     const listId = this.props.navigation.state.params.listId;
     const lists = this.props.navigation.state.params.lists;
     const {props} = this.props;
-    const {tasks,selectedIds, isOpenTaskModal,isModifyOpen} = this.state;
-    const toShow = []
+    const {
+tasks,selectedIds, isOpenTaskModal,isModifyOpen,
+} = this.state;
+    const toShow = [];
     const listLists = [];
     lists.map(
       // eslint-disable-next-line prefer-arrow-callback
@@ -120,47 +119,49 @@ class Tasks extends React.Component{
       },
     );
 
-    return(
+    return (
       <ScrollView>
         <Toolbar
-        onAdd={() => this.setState({isOpenTaskModal:true})}
-        hasSelectedIds = {selectedIds.length > 0}
-        onRemove={() => this.removeSelectedTasks()}
-        onModify={() => this.setState({isModifyOpen:true})}
-        canModify = {!(selectedIds.length == 0 || selectedIds.length > 1)}/>
-        {tasks.map(function(item,index){
+          onAdd={() => this.setState({isOpenTaskModal: true})}
+          hasSelectedIds={selectedIds.length > 0}
+          onRemove={() => this.removeSelectedTasks()}
+          onModify={() => this.setState({isModifyOpen: true})}
+          canModify={!(selectedIds.length == 0 || selectedIds.length > 1)}
+        />
+        {tasks.map(function(item){
           if (item.listId == listId){
-            toShow.push(item);}})}
+            toShow.push(item); } })}
         <Text style={styles.currentlyIn}>Currently in list {listId}</Text>
         {/* You have selected x task/s*/}
         <Text style={styles.caption}>{this.displayCaption()}</Text>
         <TaskList
-        tasks={toShow}
-        props={props}
-        onLongPress= {(taskId) => this.onTaskLongPress(taskId)}
-        selectedIds= {selectedIds}/>
+          tasks={toShow}
+          props={props}
+          onLongPress={(taskId) => this.onTaskLongPress(taskId)}
+          selectedIds={selectedIds}
+        />
 
         <CreateTask
           isOpen={isOpenTaskModal}
           closeModal={() => this.setState({isOpenTaskModal: false})}
           addTask={(info) => this.addTask(info)}
           listId={listId}
-          lists = {lists}
+          lists={lists}
 
         />
 
         <ModifyTask
-        isOpen={isModifyOpen}
-        closeModal={() => this.setState({isModifyOpen: false})}
-        addTask={(info) => this.updateTask(info)}
-        listId={listId}
-        listOptions = {listLists}
-        tasks = {tasks}
-        taskId = {selectedIds[0]}
+          isOpen={isModifyOpen}
+          closeModal={() => this.setState({isModifyOpen: false})}
+          addTask={(info) => this.updateTask(info)}
+          listId={listId}
+          listOptions={listLists}
+          tasks={tasks}
+          taskId={selectedIds[0]}
 
         />
       </ScrollView>
-    )
+    );
   }
 }
 export default Tasks;
