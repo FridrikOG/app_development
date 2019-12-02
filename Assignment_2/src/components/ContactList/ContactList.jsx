b1453f7259b13fd39eadc22e7a10ab60835f40ec
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-12-02 15:28:21 +0000

    fixed minor css error

--- Assignment_2/src/components/ContactList/ContactList.jsx
+++ Assignment_2/src/components/ContactList/ContactList.jsx
@@ -17,14 +17,12 @@ const ContactList = ({ contacts }) => (
       data={contacts}
       style={styles.flatlist}
       renderItem={({ item: { name, image } }) => (
-        <Gradient gradient="linear-gradient(160deg,#eaf7f1,#d1e8e7,#bcd9e1,#aec7db,#9bc3e1,#68ceea,#19d8dd,#09dfb9">
-          <View style={styles.contact}>
-            <ImageThumbnail style={styles.image} file={image} />
-            <Text style={styles.name}>
-              {name}
-            </Text>
-          </View>
-        </Gradient>
+        <View style={styles.contact}>
+          <ImageThumbnail style={styles.image} file={image} />
+          <Text style={styles.name}>
+            {name}
+          </Text>
+        </View>
       )}
       keyextractor={() => contacts.id}
     />
