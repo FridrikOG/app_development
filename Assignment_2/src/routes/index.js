a7d13828fcc5cc64f499e37722268606d8d14b3f
Friðrik Gunnarsson <fridrik@gmx.com>
2019-12-02 11:33:14 +0000

    dsflasdlfadslf

--- Assignment_2/src/routes/index.js
+++ Assignment_2/src/routes/index.js
@@ -1,18 +1,10 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import Main from '../views/Main';
-import Boards from '../views/Boards';
-import Lists from '../views/Lists';
-import Administrators from '../views/Administrators';
-import Tasks from '../views/Tasks';
+import Contacts from '../views/Contacts';
 
 const StackNavigator = createStackNavigator({
-  Main,
-  Boards,
-  Lists,
-  Tasks,
-  Administrators,
-});
+  Contacts
+})
 
 export default createAppContainer(StackNavigator);
---
+++ Assignment_2/src/views/Contacts/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ScrollView, View, Text } from 'react-native';
+// import Toolbar from '../../components/Toolbar';
+// import data from '../../resources/data';
+// import AdministratorList from '../../components/AdministratorList';
+
+
+class Contacts extends React.Component{
+
+      //const props = this.props.navigation.state.params;
+
+      render(){
+        return(
+          <View> 
+            <Text> le goooooo</Text>
+          </View>
+        );
+    }
+  }
+
+  export default Contacts;
