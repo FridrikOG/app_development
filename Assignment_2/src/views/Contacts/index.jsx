627be7cde836a5af6d795a6788edabc9917219e4
Friðrik Gunnarsson <fridrik@gmx.com>
2019-12-02 11:37:41 +0000

    sdfsfsadf

--- Assignment_2/src/views/Contacts/index.jsx
+++ Assignment_2/src/views/Contacts/index.jsx
@@ -1,21 +1,20 @@
+/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { View, Text } from 'react-native';
 // import Toolbar from '../../components/Toolbar';
 // import data from '../../resources/data';
 // import AdministratorList from '../../components/AdministratorList';
 
 
-class Contacts extends React.Component{
-
-      //const props = this.props.navigation.state.params;
-
-      render(){
-        return(
-          <View> 
-            <Text> le goooooo</Text>
-          </View>
-        );
-    }
+class Contacts extends React.Component {
+  // const props = this.props.navigation.state.params;
+  render() {
+    return (
+      <View>
+        <Text> le goooooo</Text>
+      </View>
+    );
   }
+}
 
-  export default Contacts;
+export default Contacts;
