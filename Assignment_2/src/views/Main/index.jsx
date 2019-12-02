a5f99921225d321db149aac900c3484fc3dbde76
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-12-02 12:56:54 +0000

    Main style added

--- Assignment_2/src/views/Main/index.jsx
+++ Assignment_2/src/views/Main/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {
-  View, Text
+  View, Text, Image, TouchableHighlight, ImageBackground,
 } from 'react-native';
 
-// import logo from '../../resources/phone.png';
-// import styles from './styles';
+import logo from '../../resources/phone.png';
+import styles from './styles';
 // import data from '../../resources/data';
 
 class Main extends React.Component {
   render() {
     return (
-      <View>
-        <Text> le goooooo</Text>
+      <View style={styles.container}>
+        <ImageBackground style= { styles.backgroundImage } source={{uri: 'https://ph-files.imgix.net/651adc69-1ed9-47b3-8f6f-3a83ed0e2322?auto=format%2Ccompress%2Cenhance&dpr=2&crop=false&fit=max&w&h=500'}}>
+          <View style={styles.logoBorder}>
+            <Image style={styles.logo} source={logo} />
+          </View>
+          <Text style={styles.title}>Mr. Contact Johnson</Text>
+          <TouchableHighlight style={styles.button}>
+            <Text style={styles.buttonText}>View Contact List</Text>
+          </TouchableHighlight>
+        </ImageBackground>
       </View>
     );
   }
