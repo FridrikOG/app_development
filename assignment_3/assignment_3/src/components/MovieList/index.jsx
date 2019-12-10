adcdf2682c6c7c60867b7a50a0a9c9838ed18b03
SofiaBasilio17 <sofiabasiliovs@gmail.com>
2019-12-10 14:41:57 +0000

    linting

--- assignment_3/assignment_3/src/components/MovieList/index.jsx
+++ assignment_3/assignment_3/src/components/MovieList/index.jsx
@@ -4,46 +4,41 @@ import {
 } from 'react-native';
 import { connect } from 'react-redux';
 import styles from './styles';
-import pop from '../../resources/images/box-of-popcorn.png';
-import bg from '../../resources/images/movie-film.png';
 
 const movieInformation = (movie) => {
-    newMovieInfo = {
-        thumbnail : movie.poster,
-        title : movie.title,
-        Released: movie.omdb[0].Released,
-        genres: movie.genres
-    }
-    return newMovieInfo;
-}
-
-
+  const newMovieInfo = {
+    thumbnail: movie.poster,
+    title: movie.title,
+    Released: movie.omdb[0].Released,
+    genres: movie.genres
+  }
+  return newMovieInfo;
+};
 
 const MovieList = (props) => {
-  let moviesBelongingToCinema = []
-  let movieList = []
-  for (x in props.movies){
-      for (y in props.movies[x].showtimes){
-          if (props.movies[x].showtimes[0].cinema.id === props.cinemaId ){
-              let newMovie = movieInformation(props.movies[x])
-              if (movieList.indexOf(newMovie.title) === -1){
-                movieList.push(newMovie.title)
-                moviesBelongingToCinema.push(newMovie)
-              }
-          }
-      } 
+  let moviesBelongingToCinema = [];
+  let movieList = [];
+  for (x in props.movies) {
+    for (y in props.movies[x].showtimes) {
+      if(props.movies[x].showtimes[0].cinema.id === props.cinemaId ) {
+        let newMovie = movieInformation(props.movies[x]);
+        if(movieList.indexOf(newMovie.title) === -1) {
+          movieList.push(newMovie.title);
+          moviesBelongingToCinema.push(newMovie);
+        }
+      }
+    }
   }
-
-
   // console.log("LOGGING MOVIES: ", moviesBelongingToCinema)
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>
-        Welcome to the cinema of ID {props.cinemaId}
+        Welcome to the cinema of ID
+        {props.cinemaId}
       </Text>
       <FlatList
         numColumns={2}
-        data={moviesBelongingToCinema }
+        data={moviesBelongingToCinema}
         style={styles.flatList}
         renderItem={({
           item: {
@@ -66,12 +61,6 @@ const MovieList = (props) => {
 // onPress={() => props.navigate('CinemaDetails', { cinemaId: id })}
 
 const mapStateToProps = (reduxStoreState) => {
-    // console.log("LOGGING MOVIES : ", reduxStoreState)
-    // console.log("Logging movie: ", reduxStoreState.movies[0].showtimes[0].cinema.id)
-    
-    //console.log("All the movies belongign to the cinema :" ,moviesBelongingToCinema)
-    
-
   return {
     cinema: reduxStoreState.cinema,
     movies: reduxStoreState.movies,
--- assignment_3/assignment_3/src/views/CinemaDetails/index.jsx
+++ assignment_3/assignment_3/src/views/CinemaDetails/index.jsx
@@ -1,40 +1,28 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import PropTypes from 'prop-types';
-import axios from 'axios';
+// import PropTypes from 'prop-types';
 import {
-  View, Text,
+  ScrollView,
 } from 'react-native';
 // import data from '../../resources/data';
-import data from '../../resources/cinemas';
-import MovieList from '../../components/MovieList/index';
-import CinemaHandler from '../../components/Handler/cinemaHandler';
 import { connect } from 'react-redux';
+import MovieList from '../../components/MovieList/index';
 
 class CinemaDetails extends React.Component {
   render() {
     const cinemaId = this.props.navigation.state.params.cinemaId;
-    
-    
-    // cinemas = this.props.cinemas;
-    // for (x in cinemas) {
-
-    // }
-
     return (
-      <View>
-        <Text> {this.c} </Text>
-      <MovieList cinemaId = {cinemaId}/>
-      </View>
+      <ScrollView>
+        <MovieList cinemaId={cinemaId} />
+      </ScrollView>
     );
   }
 }
 const mapStateToProps = (reduxStoreState) => {
-return {
-  cinemas: reduxStoreState.cinemas,
-}
+  return {
+    cinemas: reduxStoreState.cinemas,
+  }
 };
 
 
 export default connect(mapStateToProps)(CinemaDetails);
-
